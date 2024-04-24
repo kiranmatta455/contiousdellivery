dd7603ea04954339adedf2f2cdc3997105c33d9a
kiranmatta455 <159254641+kiranmatta455@users.noreply.github.com>
2024-04-24 16:44:48 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+def can_be_palindrome(s):
+    d={}
+    for i in s:
+        d[i]=d.get(i,0)+1
+        c=0
+    for v in d.values():
+
+       if v%2!=0:
+            c=c+1
+       if c>1:
+         print("No")
+       else:
+         print("yes")
+
+s="malayalam"
+can_be_palindrome(s)
+        
